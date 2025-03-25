d73a8862609fb7a76efc9e8d32c57df5faace2b7
AryanRajdev <91838247+AryanRajdev@users.noreply.github.com>
2025-03-25 22:57:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 //your code here!
+let list = document.getElementById("infi-list");
+
+function addItems(count) {
+	for(let i =0 ; i<10 ; i++){
+	   let li = document.createElement("li");
+	   li.textContent = `item-${list.children.length+1}`;
+	   list.appendChild(li);
+  }	
+}
+
+addItems(10);
+
+list.addEventListener("scroll",()=>{
+	if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+            addItems(2); // Add 2 more items when reaching the bottom
+        }
+});
 
